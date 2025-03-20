0bf75c124c732b9c210fc3387435fa349f3c65ae
torres-martinez <torres.martinez@example.com>
2025-03-20 16:40:29 -0700

    fix: 更新主入口文件

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,1 @@
-﻿import { createApp } from "vue"; import App from "./App.vue"; import router from "./router"; import "./style.css"; createApp(App).use(router).mount("#app");
+﻿import { createApp } from "vue"; import App from "./App.vue"; import router from "./router"; import "./style.css"; const app = createApp(App); app.use(router); app.mount("#app");
