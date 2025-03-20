b917a00a23d7b7404a9af692c83441cd97c4d0b4
torres-martinez <torres.martinez@example.com>
2025-03-20 16:15:43 -0700

    fix: 修复路由和应用文件

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,1 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import router from './router'
-import App from './App.vue'
-import i18n from './i18n'
-
-// 通用字体
-import 'vfonts/Lato.css'
-// 等宽字体
-import 'vfonts/FiraCode.css'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-app.use(i18n)
-
-app.mount('#app') 
+﻿import { createApp } from "vue"; import App from "./App.vue"; import router from "./router"; import "./style.css"; createApp(App).use(router).mount("#app");
---
+++ src/views/Home.vue
@@ -0,0 +1,1 @@
+﻿<template><div>Welcome to Torres Martinez种植管理系统</div></template>
