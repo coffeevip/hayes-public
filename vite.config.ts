c0a2fd59e4a6d8a9b22dd5388c19bc757e66cfd8
torres-martinez <torres.martinez@example.com>
2025-03-20 16:20:35 -0700

    fix: 修复资源路径和编码问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,1 @@
-﻿import { defineConfig } from "vite"; import vue from "@vitejs/plugin-vue"; import path from "path"; export default defineConfig({ plugins: [vue()], base: "/hayes-public/", resolve: { alias: { "@": path.resolve(__dirname, "./src") } } });
+﻿import { defineConfig } from "vite"; import vue from "@vitejs/plugin-vue"; import path from "path"; export default defineConfig({ plugins: [vue()], base: "./", resolve: { alias: { "@": path.resolve(__dirname, "./src") } }, build: { outDir: "dist", assetsDir: "assets", sourcemap: false } });
